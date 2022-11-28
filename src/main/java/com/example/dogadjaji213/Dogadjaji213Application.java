8f8509b9c261d76f55fe4c020b49c97d68c74781
MahirPrcanovic <mahirprcanovic7@gmail.com>
2022-11-28 11:57:04 +0100

    initial commit

---
+++ src/main/java/com/example/dogadjaji213/Dogadjaji213Application.java
@@ -0,0 +1,13 @@
+package com.example.dogadjaji213;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dogadjaji213Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dogadjaji213Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dogadjaji213/Dogadjaji213ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogadjaji213;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dogadjaji213ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
