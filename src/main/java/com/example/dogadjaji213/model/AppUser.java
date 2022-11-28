8ff8c0852b7f88ae6f0b6e55a745b601217de2e5
MahirPrcanovic <mahirprcanovic7@gmail.com>
2022-11-28 12:02:19 +0100

    feat: appuser and role table added

---
+++ src/main/java/com/example/dogadjaji213/model/AppUser.java
@@ -0,0 +1,25 @@
+package com.example.dogadjaji213.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private String email;
+    private String password;
+    @ManyToOne
+    @JoinColumn(name="role_id", nullable=false)
+    private Role role;
+}
